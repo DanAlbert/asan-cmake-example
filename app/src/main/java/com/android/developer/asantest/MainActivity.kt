22e42ab4af6a1075087a12625ac9bfa242194648
Dan Albert <danalbert@google.com>
2022-05-25 12:06:38 -0700

    Update to latest NDK/AGP/etc.

--- app/src/main/java/com/android/developer/asantest/MainActivity.kt
+++ app/src/main/java/com/android/developer/asantest/MainActivity.kt
@@ -2,16 +2,20 @@ package com.android.developer.asantest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.android.developer.asantest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        binding.sampleText.text = stringFromJNI()
     }
 
     /**
